f15df80193476c4566d3cdabbeee9f9f98f43c50
ngenohkevin <ngenohkevin19@gmail.com>
2023-09-04 15:12:57 +0300

    heaps

--- heaps/main.go
+++ heaps/main.go
@@ -91,10 +91,15 @@ func (h *MaxHeap) swap(i1, i2 int) {
 func main() {
 	m := &MaxHeap{}
 	fmt.Println(m)
-	buildHeap := []int{10, 20, 30}
+	buildHeap := []int{10, 20, 30, 5, 7, 9, 11, 13, 15, 17}
 
 	for _, v := range buildHeap {
 		m.Insert(v)
+		fmt.Println(m)
+	}
+
+	for i := 0; i < 5; i++ {
+		m.Extract()
 		fmt.Println(m)
 	}
 }

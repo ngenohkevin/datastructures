79b2df927bcb89f51b3c50f989ffee4ac58d6258
ngenohkevin <ngenohkevin19@gmail.com>
2023-09-04 14:39:57 +0300

    heapify

--- heaps/main.go
+++ heaps/main.go
@@ -1,1 +1,47 @@
 package main
+
+// MaxHeap struct has a slice that holds the array
+
+type MaxHeap struct {
+	array []int
+}
+
+// Inserts adds an element to the heap
+
+func (h *MaxHeap) Insert(key int) {
+	h.array = append(h.array, key)
+	h.MaxHeapifyUp(len(h.array) - 1)
+}
+
+// Extract returns the largest key, and removes it from the heap
+
+func (h *MaxHeap) MaxHeapifyUp(index int) {
+	for h.array[parent(index)] < h.array[index] {
+		h.swap(parent(index), index)
+		index = parent(index)
+	}
+}
+
+// get the parent index
+func parent(i int) int {
+	return (i - 1) / 2
+}
+
+// get the left child index
+func left(i int) int {
+	return 2*i + 1
+}
+
+// get the right child index
+func right(i int) int {
+	return 2*i + 2
+}
+
+// swap keys in the array
+func (h *MaxHeap) swap(i1, i2 int) {
+	h.array[i1], h.array[i2] = h.array[i2], h.array[i1]
+}
+
+func main() {
+
+}

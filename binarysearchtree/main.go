f386d0d3839b26274e3f44dc3cd61bcb3e24739b
ngenohkevin <ngenohkevin19@gmail.com>
2023-09-13 13:04:07 +0300

    Insert into the bst

--- binarysearchtree/main.go
+++ binarysearchtree/main.go
@@ -1,1 +1,25 @@
 package main
+
+// Node represents a single node in the BST.
+
+type Node struct {
+	Value int
+	Left  *Node
+	Right *Node
+}
+
+// BST represents the Binary Search Tree.
+
+type BST struct {
+	Root *Node
+}
+
+// Insert inserts a new value into the BST
+func (bst *BST) Insert(value int) {
+	newNode := &Node{Value: value}
+
+	if bst.Root == nil {
+		bst.Root = newNode
+		return
+	}
+}

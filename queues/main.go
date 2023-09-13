d405a238ed89fc038f0efabb5045cc0c904cb4fc
ngenohkevin <ngenohkevin19@gmail.com>
2023-09-13 11:33:24 +0300

    dequeue

--- queues/main.go
+++ queues/main.go
@@ -5,6 +5,23 @@ type Queue struct {
 }
 
 // Enqueue (push) adds an element to the rear of the queue
-func (q *Queue) Enqueue(i int) {
+func (q *Queue) Enqueue(item int) {
+	q.items = append(q.items, item)
+}
 
+// Dequeue (Pop) removes and returns the front element from the queue.
+func (q *Queue) Dequeue() int {
+	if q.IsEmpty() {
+		panic("Queue is empty")
+	}
+
+	front := q.items[0]
+	q.items = q.items[1:]
+
+	return front
+}
+
+// IsEmpty checks if the queue is empty
+func (q *Queue) IsEmpty() bool {
+	return len(q.items) == 0
 }

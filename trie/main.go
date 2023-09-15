412ee21a501c6490662a701f6f8414e7f073ec86
ngenohkevin <ngenohkevin19@gmail.com>
2023-09-15 12:14:50 +0300

    Node struct

--- trie/main.go
+++ trie/main.go
@@ -3,6 +3,11 @@ package main
 // AlphabetSize is the number of possible characters in the trie
 const AlphabetSize = 26
 
+type Node struct {
+	children [26]*Node
+	isEnd    bool
+}
+
 func main() {
 
 }
